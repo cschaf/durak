7aa8aa6448a48d662b0b19178f62422b223f0688
Christian Schaf <cschaf@outlook.com>
2015-09-28 20:27:01 +0200

    added  create game functionality

---
+++ client/home.js
@@ -0,0 +1,18 @@
+Template.userList.helpers({
+    users: function () {
+        var myid = Meteor.userId();
+        var cantPlayAgainst =  [myid];
+
+        Games.find({inProgress: true}).forEach(function(game){
+            cantPlayAgainst.push(game.currentTurn[game.currentTurn[0] === myid ? 1: 0]);
+        });
+
+        return Meteor.users.find({_id:{$not:{$in: cantPlayAgainst}}});
+    }
+});
+
+Template.userItem.events({
+    'click button': function(evt, template){
+        Meteor.call('createGame', template.data._id);
+    }
+});
